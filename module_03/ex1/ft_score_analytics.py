5000bae214e8c9431a7df43345da3059f4327bf3
yandsouza <yannssouza@outlook.com>
2026-07-24 16:26:37 -0300

    Add files via upload

--- module_03/ex1/ft_score_analytics.py
+++ module_03/ex1/ft_score_analytics.py
@@ -1,26 +1,32 @@
 import sys
 
-def ft_score_analytics() -> None:
+def validade_list(arg):
     try:
-        for arg in sys.argv[1:]:
-            int(arg)
+        int(arg)
     except ValueError as error:
-        for arg in sys.argv[1:]:
-            print(f"Invalid parameter: {arg}")
-        print("No scores provided. Usage: "
-              "python3 ft_score_analytics.py <score1> <score2> ...")
-        return
-    if len(sys.argv) == 1:
-        print("No scores provided. Usage: "
-              "python3 ft_score_analytics.py <score1> <score2> ...")
-        return
-    scores = [int(num) for num in sys.argv[1:]]
+        print(f"Invalid parameter: {arg}")
+    else:
+        return int(arg)
+
+def print_score(scores: list[int]) -> None:
     print(f"Scores processed: {scores}")
     print(f"Total players: {len(scores)}")
     print(f"Total score: {sum(scores)}")
     print(f"Average score: {sum(scores) / len(scores)}")
     print(f"Low score: {min(scores)}")
     print(f"Score range: {max(scores) - min(scores)}")
+
+def ft_score_analytics() -> None:
+    scores = []
+    for arg in sys.argv[1:]:
+        value = validade_list(arg)
+        if value is not str and value is not None:
+            scores.append(value)
+    if not scores:
+        print("No scores provided. Usage: "
+              "python3 ft_score_analytics.py <score1> <score2> ...")
+        return 
+    print_score(scores)
 
 if __name__ == "__main__":
     print("=== Player Score Analytics ===")
---
+++ module_03/ex2/ft_coordinate_system.py
@@ -0,0 +1,6 @@
+def get_player_pos():
+    cords = input("Enter new coordinates as floats in format 'x,y,z': ")
+    cords = cords.split('j')
+    print(cords)
+
+get_player_pos()

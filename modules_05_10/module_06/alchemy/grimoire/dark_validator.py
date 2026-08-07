84bc5bcbaff0a99bda869a30fe87b9c70c8f9bc7
Yann D'Souza <yannssouza@outlook.com>
2026-08-07 03:29:40 -0300

    new file

--- modules_05_10/module_06/alchemy/grimoire/dark_validator.py
+++ modules_05_10/module_06/alchemy/grimoire/dark_validator.py
@@ -1,5 +1,6 @@
 from .dark_spellbook import dark_spell_allowed_ingredients
 
+
 def validate_ingredients(ingredients: str) -> str:
     allowed = dark_spell_allowed_ingredients()
     ingredients_lower = ingredients.lower()

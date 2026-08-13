040a07c685122c0807135c02a28492ff41b69b37
Yann D'Souza <yannssouza@outlook.com>
2026-08-13 15:24:44 -0300

    module 10 init

---
+++ modules_05_10/module_10/ex0/lambda_spells.py
@@ -0,0 +1,10 @@
+def artifact_sorter(artifacts: list[dict]) -> list[dict]
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]
+
+def spell_transformer(spells: list[str]) -> list[str]
+
+def mage_stats(mages: list[dict]) -> dict
+
+if __name__ == "__main__":
+    artifact_sorter()
